439a5693010413dc1ba7ccae75438e650ad12161
leo <lele.mollard@gmail.com>
2021-02-07 18:12:36 +0100

    initial landing page

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import "assets/vendor/nucleo/css/nucleo.css";
+import "assets/vendor/font-awesome/css/font-awesome.min.css";
+import "assets/scss/argon-design-system-react.scss?v1.1.0";
+import {BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" exact render={props => <App {...props} />} />
+
+            <Redirect to="/" />
+        </Switch>
+    </BrowserRouter>,
+    document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
